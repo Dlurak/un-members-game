7c36a14f64a08b1cc91b605605f94fd5f20bd94a
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-03-25 21:19:41 +0100

    Change the shuffle algorithm

--- src/utils/arrays/random.ts
+++ src/utils/arrays/random.ts
@@ -1,5 +1,15 @@
 /**
  * Shuffles an array without mutating the original array.
  */
-export const shuffle = <T>(arr: T[]): T[] =>
-  arr.sort(() => Math.random() - 0.5);
+export const shuffle = <T>(arr: T[]) => {
+	const copy = [...arr];
+	const result: T[] = [];
+
+	while (copy.length) {
+		const randomIndex = Math.floor(Math.random() * copy.length);
+		result.push(copy[randomIndex]);
+		copy.splice(randomIndex, 1);
+	}
+
+	return result;
+}
