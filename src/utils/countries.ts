3f8fef8d132888509a9a8277b99b6e46183132d1
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-03-26 00:43:18 +0100

    Remove a debug filter

--- src/utils/countries.ts
+++ src/utils/countries.ts
@@ -22,6 +22,5 @@ export async function getCountries() {
     "https://restcountries.com/v3.1/independent?status=true&fields=name,flags,capital",
   ).then((r) => r.json());
 
-  return responseSchema.parse(data)
-		.filter(c => ["Italy", "San Marino", "Germany"].includes(c.name.common));
+  return responseSchema.parse(data);
 }
