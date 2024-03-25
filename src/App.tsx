b3ce31642f207390ff9dc9a992ed601e5bc31ecd
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-03-25 02:08:11 +0100

    Implement a score system

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,24 @@ import { shuffle } from "./utils/arrays/random";
 
 function App() {
   const [countries, setCountries] = useState<Country[]>([]);
-	
+  const [scores, setScores] = useState<Record<string, number>>({});
 
   useEffect(() => {
     getCountries().then((data) => {
       setCountries(shuffle(data));
+
+      for (const country of countries) {
+        scores[country.name.common] = 0;
+      }
     });
   }, []);
+
+  const incrementScore = (country: string) => {
+    const countryScore = scores[country] || 0;
+    const scoreCopy = { ...scores };
+    scoreCopy[country] = countryScore + 1;
+    setScores(scoreCopy);
+  };
 
   if (!countries.length || !countries[0]?.name.common) {
     return <div>Loading...</div>;
@@ -21,15 +32,19 @@ function App() {
   return (
     <>
       <Map
-		currentCountry={countries[0]?.name.common}
-		onCorrect={() => {
-		  window.alert("Correct!");
-		  setCountries(countries.slice(1));
-		}}
-		onIncorrect={() => {
-		  window.alert("Incorrect!");
-		}}
-	  />
+        currentCountry={countries[0]?.name.common}
+        onCorrect={() => {
+          window.alert("Correct!");
+          setCountries(countries.slice(1));
+
+          incrementScore(countries[0].name.common);
+        }}
+        onIncorrect={() => {
+          window.alert("Incorrect!");
+
+          incrementScore(countries[0].name.common);
+        }}
+      />
       <QuestionBox
         country={countries[0]?.name.common}
         capital={countries[0].capital[0]}
